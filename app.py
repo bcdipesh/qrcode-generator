b21152f99e9437335986765ee9e65137c0122450
Dipesh B C <bcdipeshwork@gmail.com>
2023-08-27 19:56:04 -0400

    Update form

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
+
 from dotenv import load_dotenv
-
-from models import User, QR_Code, QR_Code_Usage_Statistics, connect_db, db
-
 from flask import Flask, render_template
+
+from forms import QRCodeForm
+from models import QR_Code, QR_Code_Usage_Statistics, User, connect_db, db
 
 # Load environment variables
 load_dotenv()
@@ -14,6 +15,7 @@ app.config[
     "SQLALCHEMY_DATABASE_URI"
 ] = f"postgresql://postgres:{os.environ.get('DB_PASSWORD')}@localhost/qrcode_generator"
 app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "QRCode Generator"
 
 connect_db(app)
 
@@ -27,5 +29,20 @@ connect_db(app)
 @app.route("/")
 def home_page():
     """Display home page"""
+    
+    form = QRCodeForm()
 
-    return render_template("home.html")
+    if form.validate_on_submit():
+        # Process the form data
+        content = form.content.data
+        size = form.size.data
+        charset_source = form.charset_source.data
+        charset_target = form.charset_target.data
+        ecc = form.ecc.data
+        color = form.color.data
+        bg_color = form.bg_color.data
+        margin = form.margin.data
+        qzone = form.qzone.data
+        file_format = form.file_format.data
+
+    return render_template("home.html", form=form)
--- forms.py
+++ forms.py
@@ -1,18 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, RadioField, SelectField, IntegerField
+from wtforms import IntegerField, RadioField, SelectField, StringField
 from wtforms.validators import DataRequired
 
 
 class QRCodeForm(FlaskForm):
     """Form to gather data to generate qr code"""
 
-    data = StringField("Data", validators=[DataRequired()])
+    content = StringField("Content", validators=[DataRequired()])
     size = StringField("Size")
-    charset_source = RadioField("Charset Source")
-    charset_target = RadioField("Charset Target")
-    ecc = SelectField("Error Correction Code")
+    charset_source = RadioField("Charset Source", choices=[("ISO-8859-1", "ISO-8859-1"), ("UTF-8", "UTF-8")], default="UTF-8")
+    charset_target = RadioField("Charset Target", choices=[("ISO-8859-1", "ISO-8859-1"), ("UTF-8", "UTF-8")], default="UTF-8")
+    ecc = SelectField("Error Correction Code", choices=[("L", "L"), ("M", "M"), ("Q", "Q"), ("H", "H")], default="L")
     color = StringField("Color")
     bg_color = StringField("Background Color")
     margin = IntegerField("Margin")
     qzone = IntegerField("Margin Thickness")
-    format = RadioField("File Format")
+    file_format = RadioField("File Format", choices=[("PNG", "png"), ("GIF", "gif"), ("JPEG", "jpeg"), ("JPG", "jpg"), ("SVG", "svg"), ("EPS", "eps")], default="PNG")
