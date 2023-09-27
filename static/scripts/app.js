2f8c91bc5440bca5f4786854aa410b16ab0fc658
Dipesh B C <bcdipeshwork@gmail.com>
2023-09-26 21:57:21 -0400

    Add ability to modify a qrcode

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -18,11 +18,25 @@ createQRBtn.addEventListener('click', async function (event) {
 	const ecc = createQRCodeForm.elements['ecc'].value;
 	const color = createQRCodeForm.elements['color'].value;
 	const bgColor = createQRCodeForm.elements['bg_color'].value;
-	const margin = createQRCodeForm.elements['margin'].value;
-	const qzone = createQRCodeForm.elements['qzone'].value;
+	// const margin = createQRCodeForm.elements['margin'].value;
+	// const qzone = createQRCodeForm.elements['qzone'].value;
 	fileFormat = createQRCodeForm.elements['file_format'].value;
 
-	qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?data=${content}`;
+	const url = new URL('https://api.qrserver.com/v1/create-qr-code');
+	url.searchParams.set('data', content);
+	url.searchParams.set('size', size);
+	url.searchParams.set('charset-source', charsetSource);
+	url.searchParams.set('charset-target', charsetTarget);
+	url.searchParams.set('ecc', ecc);
+	url.searchParams.set('color', color);
+	url.searchParams.set('bgcolor', bgColor);
+	// url.searchParams.set('margin', margin);
+	// url.searchParams.set('qzone', qzone);
+	url.searchParams.set('format', fileFormat);
+
+	qrCodeUrl = url.href;
+
+	console.log(url);
 
 	// Update QR Code image
 	document.querySelector('.qrcode-img').src = qrCodeUrl;
